f7f1fad9c4cdf040f768aa7e2e2c74a29b06ed43
AdamTorrey <21atorrey@gmail.com>
2024-09-03 20:24:31 -0400

    Worked Ahead a Little
    Did a bit of coding ahead of the class (this is fun)

--- ApplePicker/Assets/AppleTree.cs
+++ ApplePicker/Assets/AppleTree.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+    [Header("Inscribed")]
+    // Prefab for instantiating apples
+    public GameObject applePrefab;
+
+    // Speed at which the AppleTree moves
+    public float speed = 1f;
+
+    // Distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+
+    //chance that the AppleTree will change directions
+    public float changeDirChance = 0.1f;
+
+    // Seconds between Apples instantiations
+    public float appleDropDelay = 1f;
+
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        // Start Dropping Apples
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        // Basic Movement
+        // Changing Direction
     }
 }
